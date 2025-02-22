ecaf0e5a144055727ece8cb7256c8f40e89ab26b
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2025-02-22 09:09:59 +0700

    Update Cloudflare.py

--- Optional/Cloudflare.py
+++ Optional/Cloudflare.py
@@ -1,5 +1,39 @@
-import os, sys
-import seledroid
-for seledroid import bypassCloudflare
+import os, sys, time, json
+from seledroid import webdriver
+from seledroid.webdriver.common.by import By
 
-# ini tes wkwk
+driver = webdriver.Chrome(gui=False)
+host = sys.argv[1]
+
+def Cloudflare():
+	title = driver.title
+	if any(sub.lower() in title.lower() for sub in ["cloudflare","just a moment..."]):
+		time.sleep(10)
+		return False
+	else:
+		return True
+
+try:
+	driver.get(host)
+	while not Cloudflare():
+		time.sleep(3)
+	
+	cf_clearance = driver.get_cookie("cf_clearance")
+	user_agent = driver.user_agent
+except Exception as e:
+	print(f"{e}")
+finally:
+	title = driver.title
+	if any(sub.lower() in title.lower() for sub in ["cloudflare","just a moment..."]):
+		data = {
+		"cf_clearance" : False,
+		"user-agent" : user_agent
+		}
+	else:
+		data = {
+		"cf_clearance" : cf_clearance.split("=")[1],
+		"user-agent" : user_agent
+		}
+	with open('cf.json', 'w') as file:
+		json.dump(data, file, indent=4)
+	driver.close()
